c0c02ad6e8d6b07720540c5f2752a4ce3abbf0ae
Usman Kabeer <usman@gmail.com>
2021-04-16 23:31:48 +0500

    Changed division to addition.

--- My Project/My Project/main.cpp
+++ My Project/My Project/main.cpp
@@ -7,12 +7,5 @@ int main()
 	double x, y;
 	cout << "Enter two numbers: ";
 	cin >> x >> y;
-	if (y == 0)
-	{
-		cout << "You can't enter zero as the denominator" << endl;
-	}
-	else
-	{
-		cout << "x / y: " << x / y << endl;
-	}
+	cout << "x + y: " << x + y << endl;
 }
